e8b79521571a4f3ed7633b9237a18e4ac63188e0
ss473901 <satoshi.473901.1205@gmail.com>
2022-01-06 18:50:47 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./components/Button";
+import Result from "./components/Result";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="result">
+        <Result result={"計算結果"} />
+      </div>
+      <div className="button-wrapper">
+        <div className="number">
+          <div>
+            <div className="upper">
+              <Button text={"7"} />
+              <Button text={"8"} />
+              <Button text={"9"} />
+            </div>
+            <div className="middle">
+              <Button text={"4"} />
+              <Button text={"5"} />
+              <Button text={"6"} />
+            </div>
+            <div className="lower">
+              <Button text={"1"} />
+              <Button text={"2"} />
+              <Button text={"3"} />
+            </div>
+            <div className="buttom">
+              <Button text={"0"} />
+              <Button text={"AC"} />
+              <Button text={"="} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Button />
+      <Result />
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Result = ({ result }) => {
+  return (
+    <div className="resultValue">
+      <p>{result}</p>
+    </div>
+  );
+};
+
+export default Result;
